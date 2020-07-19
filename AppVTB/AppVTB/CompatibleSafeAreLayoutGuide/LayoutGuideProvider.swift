29a79ec810d35c6612127a891a8d6c94c3e51ed2
Maxim Andryushin <unlimimage@yandex.ru>
2020-07-19 04:02:12 +0300

    Added CompatibleSafeArea

---
+++ AppVTB/AppVTB/CompatibleSafeAreLayoutGuide/LayoutGuideProvider.swift
@@ -0,0 +1,37 @@
+//
+//  LayoutGuideProvider.swift
+//  HomeWork10
+//
+//  Created by Maxim Andryushin on 12.07.2020.
+//  Copyright © 2020 Maxim Andryushin. All rights reserved.
+//
+
+import UIKit
+
+//to support ios10
+
+protocol LayoutGuideProvider {
+    var leadingAnchor: NSLayoutXAxisAnchor { get }
+    var trailingAnchor: NSLayoutXAxisAnchor { get }
+    var leftAnchor: NSLayoutXAxisAnchor { get }
+    var rightAnchor: NSLayoutXAxisAnchor { get }
+    var topAnchor: NSLayoutYAxisAnchor { get }
+    var bottomAnchor: NSLayoutYAxisAnchor { get }
+    var widthAnchor: NSLayoutDimension { get }
+    var heightAnchor: NSLayoutDimension { get }
+    var centerXAnchor: NSLayoutXAxisAnchor { get }
+    var centerYAnchor: NSLayoutYAxisAnchor { get }
+}
+
+extension UIView: LayoutGuideProvider { }
+extension UILayoutGuide: LayoutGuideProvider { }
+
+extension UIView {
+    var compatibleSafeAreaLayoutGuide: LayoutGuideProvider {
+        if #available(iOS 11, *) {
+            return safeAreaLayoutGuide
+        } else {
+            return self
+        }
+    }
+}
